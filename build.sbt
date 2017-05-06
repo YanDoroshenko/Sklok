c52e210642c90cd2c1ee1abad74402b5a33e97e3
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-05-07 01:16:20 +0300

    [new] Running HelloWorld application

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "sKlok"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.11"
+
+enablePlugins(AndroidApp)
+
+platformTarget := "25"
+
+useProguard in Android := true
---
+++ src/main/scala/org/sklok/TemplateActivity.scala
@@ -0,0 +1,10 @@
+package org.sklok
+
+import android.app.Activity
+
+/**
+  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 07.05.2017.
+  */
+class TemplateActivity extends Activity {
+
+}
